513ad1af54ce7880bbb89acbba7bb418b4706d0d
Эвгеній  Кобахідзе <seraphjackson@gmail.com>
2021-12-12 14:06:02 +0200

    Added bottom menu and fragments

--- app/src/main/java/com/example/starwars/MainActivity.kt
+++ app/src/main/java/com/example/starwars/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.starwars
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val mainContainer = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        val navController = mainContainer.navController
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.starWarsList, R.id.favourites))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/example/starwars/StarWarsList.kt
@@ -0,0 +1,11 @@
+package com.example.starwars
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class StarWarsList : Fragment(R.layout.fragment_star_wars_list) {
+
+}
